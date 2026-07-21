6668d9d18825a7ca659f006c2b1964f909e8e4bf
Andrew Siwko <asiwko@siwko.org>
2026-07-20 21:15:25 -0400

    let's send a single json message per reading

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-exec rtl_433 -F "mqtt://${MQTT_HOST}:${MQTT_PORT},retain=0,devices=${MQTT_TOPIC_PREFIX}/[model]/[id],events=${MQTT_TOPIC_PREFIX}/events/[model]/[id]" -F json "$@"
+exec rtl_433 -F "mqtt://${MQTT_HOST}:${MQTT_PORT},retain=0,events=${MQTT_TOPIC_PREFIX}/json/" "$@"
