4c7907d7cf5243456c4ac147f766beb2856bd417
Andrew Siwko <asiwko@siwko.org>
2026-07-21 08:08:06 -0400

    removed / on the queue name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-exec rtl_433 -F "mqtt://${MQTT_HOST}:${MQTT_PORT},retain=0,events=${MQTT_TOPIC_PREFIX}/json/" "$@"
+exec rtl_433 -F "mqtt://${MQTT_HOST}:${MQTT_PORT},retain=0,events=${MQTT_TOPIC_PREFIX}/json" "$@"
