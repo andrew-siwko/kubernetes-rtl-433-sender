8ffc1a1cdc2fece233dd5c6676b3450ef77b05a3
Andrew Siwko <asiwko@siwko.org>
2026-07-22 09:26:46 -0400

    add some statistics.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-exec rtl_433 -F "mqtt://${MQTT_HOST}:${MQTT_PORT},retain=0,events=${MQTT_TOPIC_PREFIX}/json" "$@"
+exec rtl_433 -M level -M protocol -M noise -F "mqtt://${MQTT_HOST}:${MQTT_PORT},retain=0,events=${MQTT_TOPIC_PREFIX}/json" "$@"
